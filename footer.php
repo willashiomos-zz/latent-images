3626cfc17aba3b4969680b3c04f7f304d776ecf0
Willa Shiomos <willashiomos@Willas-MacBook-Air.local>
2020-06-07 16:45:27 -0400

    a lot of home page fixes

--- footer.php
+++ footer.php
@@ -1,21 +1,55 @@
 <?php
 /**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
+ * FOOTER
  * @package latentimages
  */
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-		
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<div class="w-100 footer d-flex align-items-center">
+	<div class="container">
+		<div class="row justify-content-center mb-4">
+			<img class="col-auto" width="94px" height="74px" src="wp-content/themes/latentimages/img/logo.png"></img>
+		</div>
+		<div class="row justify-content-center align-items-center">
+			<div class="col-auto">
+				<div class="row">
+					<div class="col-md-7">
+						<a href="/latest">The Latest</a>
+					</div>
+					<div class="col-sm-5">
+						<a href="/editorials">Editorials</a>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-md-7">
+						<a href="/reviews">Reviews</a>
+					</div>
+					<div class="col-sm-5">
+						<a href="/features">Features</a>
+					</div>
+				</div>
+			</div>
+				
+			<div class="col-auto mx-4">
+				<div class="vertical-line"></div>
+			</div>
+
+			<div class="col-auto">
+				<div class="row">
+					<div class="col-sm-12">
+						<a href="/about">About Us</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<a href="/contact">Contact</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		function wrapped() {
+			$('.entry-title').each(function() {
+				if($(this).height() > 110) {
+					var lines = $(this).height()/96;
+					var marginTop = 220 - 40*lines;
+				}
+				$(this).css({'margin-top': marginTop});
+				
+			});
+
+		}
+		$(document).ready(function() {
+			wrapped();
+			$('.opacity-overlay').hover(function(){
+				$(this).prev('.entry-title-right').css({'text-decoration': 'underline'});
+				$('.entry-title-right a:link').css({'text-decoration-color': '#506CCF'});
+			}, function() {
+				$(this).prev('.entry-title-right').css('text-decoration', 'none');
+			});
+		});
+	</script>
+</head>
+
 <div class="body">
 	<?php
 	/**
@@ -9,8 +35,10 @@
 	?>
 		<div class="home-container px-5">
 			<div class="section-title row">
-				Reviews
+				<div class="col-auto">Reviews</div>
+				<img class="col-auto align-self-center pink-curved-arrow" width="83px" height="100px" src="wp-content/themes/latentimages/img/curved_pink_arrow.png"></img>
 			</div>
+			
 
 			<?php $reviews = new WP_Query(array(
 				'cat' => get_cat_ID('Reviews'),
@@ -18,24 +46,28 @@
 			));
 
 			$index = 0;?>
-			<div class="section-content section-content-left row">
+			<div class="section-content section-content-left row mb-5">
 				<?php if( $reviews->have_posts() ) :
 					while( $reviews->have_posts() and $index < 2 ):
 						$reviews->the_post();?>
-						<div class="mb-5 row">
-							<div class="opacity-overlay">
+						<div class="mb-5 row no-gutters">
+							<div class="opacity-overlay col-auto">
 								<a href="<?php the_permalink(); ?>">
-									<?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full homepage-thumbnail', 'title' => 'Feature image']);?>
+									<?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full homepage-thumbnail']);?>
 								</a>
 							</div>
-							<?php the_title( '<div class="entry-title entry-title-left"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></div>' );?>
+							<?php 
+								$title = get_the_title();
+								$title = mb_strimwidth($title, 0, 40, '...');
+							?>
+							<div class="entry-title entry-title-left col"><a href="' . esc_url( get_permalink() ) . '"><?php echo $title ?></a></div>		
 						</div>
 						<?php $index++;
 					endwhile;
 				endif;?>
 
 			<a href="/reviews" class="row justify-content-end text-decoration-none" style="width:450px">
-				<div class="col-auto see-more">See More</div>
+				<div class="col-auto see-more text-pink">See More</div>
 				<img class="col-auto align-self-center" width="72px" height="23px" src="wp-content/themes/latentimages/img/right_pink_arrow.png"></img>
 			</a>
 		</div>
@@ -43,8 +75,9 @@
 			<?php wp_reset_postdata();?>
 
 			<div class="text-right">
-				<div class="section-title">
-					Editorials
+				<div class="section-title row justify-content-end">
+					<img class="col-auto align-self-center blue-curved-arrow" width="83px" height="100px" src="wp-content/themes/latentimages/img/curved_blue_arrow.png"></img>
+					<div class="col-auto">Editorials</div>
 				</div>
 
 				<?php
@@ -54,15 +87,19 @@
 					));
 
 					$index = 0;?>
-					<div class="section-content section-content-right row justify-content-end">
+					<div class="section-content section-content-right row justify-content-end mb-5">
 						<?php if( $editorials->have_posts() ) :
 							while( $editorials->have_posts() and $index < 2 ):
 								$editorials->the_post();?>
-								<div class="mb-5 row">
-									<?php the_title( '<div class="entry-title entry-title-right"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></div>' );?>
-									<div class="opacity-overlay">
+								<div class="mb-5 row no-gutters">
+									<?php 
+									$title = get_the_title();
+									$title = mb_strimwidth($title, 0, 40, '...');
+									?>
+									<div class="entry-title entry-title-right col"><a href="' . esc_url( get_permalink() ) . '"><?php echo $title ?></a></div>	
+									<div class="opacity-overlay col-auto">
 										<a href="<?php the_permalink(); ?>">
-											<?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full homepage-thumbnail', 'title' => 'Feature image']);?>
+											<?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full homepage-thumbnail']);?>
 										</a>
 									</div>
 									
@@ -70,13 +107,18 @@
 								<?php $index++;
 							endwhile;
 						endif;?>
+						<a href="/editorials" class="row justify-content-end text-decoration-none" style="width:450px">
+							<div class="col-auto see-more text-blue">See More</div>
+							<img class="col-auto align-self-center" width="72px" height="23px" src="wp-content/themes/latentimages/img/right_blue_arrow.png"></img>
+						</a>
 					</div>
 					
 					<?php wp_reset_postdata();?>
 			</div>
 
-			<div class="section-title">
-				Features
+			<div class="section-title row">
+				<div class="col-auto">Features</div>
+				<img class="col-auto align-self-center pink-curved-arrow" width="83px" height="100px" src="wp-content/themes/latentimages/img/curved_pink_arrow.png"></img>
 			</div>
 		
 			
@@ -88,23 +130,27 @@
 
 				$index = 0;?>
 
-				<div class="section-content section-content-left row">
+				<div class="section-content section-content-left row mb-5">
 					<?php if( $features->have_posts() ) :
 						while( $features->have_posts() and $index < 2 ):
 							$features->the_post();?>
-							<div class="mb-5 row">
-								<div class="opacity-overlay">
+							<div class="mb-5 row no-gutters">
+								<div class="opacity-overlay col-auto">
 									<a href="<?php the_permalink(); ?>">
-										<?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full homepage-thumbnail', 'title' => 'Feature image']);?>
+										<?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive responsive--full homepage-thumbnail']);?>
 									</a>
 								</div>
-								<?php the_title( '<div class="entry-title entry-title-right"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></div>' );?>
+								<?php 
+									$title = get_the_title();
+									$title = mb_strimwidth($title, 0, 40, '...');
+								?>
+								<div class="entry-title entry-title-left col"><a href="' . esc_url( get_permalink() ) . '"><?php echo $title ?></a></div>		
 							</div>
 							<?php $index++;
 						endwhile;
 					endif;?>
 					<a href="/features" class="row justify-content-end text-decoration-none" style="width:450px">
-						<div class="col-auto see-more">See More</div>
+						<div class="col-auto text-pink see-more">See More</div>
 						<img class="col-auto align-self-center" width="72px" height="23px" src="wp-content/themes/latentimages/img/right_pink_arrow.png"></img>
 					</a>
 				</div>
